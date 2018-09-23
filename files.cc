ff0167db168fa5ec82d94d529fa2879431f2db8a
Julianheine97441 <33298408+Julianheine97441@users.noreply.github.com>
2018-09-23 17:05:20 -0300

    Add files via upload

--- files.cc
+++ files.cc
@@ -11,13 +11,10 @@ int print_file(ifstream &);
 int main(void)
 {
 	ifstream infile; 
-	Array<Sensor<double>> * red;
+	Array<Sensor<Data<double>>> * red;
 	string * string_array;
 	size_t	 len;
-	size_t i,j;
-	int check;
-	/*double datos[3][3];
-	char ch;*/
+	size_t i;
 
    	infile.open("/home/julian/Desktop/Algo II/TP0/data.txt",ios::in);
 
@@ -25,26 +22,24 @@ int main(void)
 	
 	cout << "---------------------String Array---------------------" << endl;
 	for(i=0;i<len;i++) cout << string_array[i] << endl;
-	cout << "------------------------------------------------------" << endl;
 
 
 	cout << "----------------CreateFromIDArray---------------------" << endl;
 
 	CreateFromIDArray(red,string_array,len);
 
-	cout << "------------------------------------------------------" << endl;
 
 	cout << "---------------------Sensor Array---------------------" << endl;
 	for(i=0;i<len;i++) cout << (*red)[i].GetID() << endl;
-	cout << "------------------------------------------------------" << endl;
+
 	
 	cout << "-----------------------Get data-----------------------" << endl;
 	get_data(infile,red,',',len);
-	cout << "------------------------------------------------------" << endl;
+
 
 	cout << "---------------------Data vectors---------------------" << endl;
 	for(i=0;i<len;i++) cout << (*red)[i].GetArray() << endl;
-	cout << "------------------------------------------------------" << endl;
+
 
 	infile.close();
 
@@ -70,59 +65,4 @@ int print_file(ifstream & infile){
 	return 0;
 }
 
-
-
-/*****************************  ALMACENAMIENTO DE DATOS  ************************************/
-
-/*int get_data(ifstream & infile,double string_vector[][3],char delimiter,int len)
-{
-
-	char ch;
-	bool bad = false;
-	bool good = false;
-	bool endline = false;
-	double data;
-	int i=0;
-	int j=0;
-
-
-	while(!infile.eof()){
-
-
-		if(i == len) return 0;		
-	
-		if(infile >> data)
-		{
-			if(infile >> ch && ch == delimiter)
-				good = true;
-
-			else if(infile.good()){
-				infile.putback(ch);				
-				endline =true;
-				good=true;
-			}
-		}
-		
-		else bad = true;
-
-		if(good)
-			 string_vector[i][j] = data;	
-					
-		if(bad){
-			 cout << "NO DATA" << endl;
-			 return 1;
-		}
-			 
-		j++;
-		
-		if(endline == true){
-			
-			i++;
-			j=0;
-			endline = false;
-		}
-	}
-
-	return 0;
-}*/
 
