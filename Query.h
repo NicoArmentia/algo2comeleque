e2fff316838220990d9be76883012a0106724df6
Julianheine97441 <33298408+Julianheine97441@users.noreply.github.com>
2018-09-24 22:10:44 -0300

    Add files via upload

--- Query.h
+++ Query.h
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
+#include <fstream>
 #include "Array.h"
 
 using namespace std;
 
-#ifndef _ARRAY_H_INCLUDED_
-#define _ARRAY_H_INCLUDED_
+#ifndef _QUERY_H_INCLUDED_
+#define _QUERY_H_INCLUDED_
 
 typedef enum {OK,BAD_QUERY,NO_DATA,UNKNOWN_ID} qstate_t;
 
@@ -28,11 +29,91 @@ public:
 	Query(const string * &);
 	Query(const string * &,size_t,size_t);  
 
-	SetID
-	GetID
+	void SetID(const string &);
+	const string & GetID()const;
 	
+	void SetInitPos(size_t);
+	size_t GetInitPos() const;
+
+	void SetFinPos(size_t);
+	size_t GetFinPos() const;
+
+	void CalcMin();
+	const T & GetMin() const;
+
+	void CalcMax();
+	const T & GetMax() const;
+
+	void CalcProm();
+	const T & GetProm() const;
+
+	void SetState(const qstate_t &);
+	const qstate_t & GetState();
+
+	void SetQuery(ifstream &,char delimiter);
+
 };
 
+/**************************************** METODOS DE CLASE ****************************************/
+
+template <typename T>
+void Query<T>::SetID(const string & s){IDs = s;}
+
+template <typename T>
+const string & Query<T>::GetID()const{return IDs;}
+
+template <typename T>
+void Query<T>::SetInitPos(size_t pos){init_pos = pos;}
+
+template <typename T>
+size_t Query<T>::GetInitPos() const{return init_pos;}
+
+template <typename T>
+void Query<T>::SetFinPos(size_t pos){fin_pos = pos;}
+
+template <typename T>
+size_t Query<T>::GetFinPos() const{return fin_pos;}
+
+template <typename T>
+const T & Query<T>::GetMin() const{return min;}
+
+template <typename T>
+const T & Query<T>::GetMax() const{return max;}
+
+template <typename T>
+const T & Query<T>::GetProm() const{return prom;}
+
+template <typename T>
+void Query<T>::SetState(const qstate_t & new_state){state = new_state;}
+
+template <typename T>
+const qstate_t & Query<T>::GetState(){return state;}
+
+template <typename T>
+void Query<T>::SetQuery(ifstream & infile,char delimiter){
+
+	char ch;
+	size_t aux_init;
+	size_t aux_fin;
+	string aux_s;
+
+
+	if(getline(infile,aux_s,delimiter) && infile >> aux_init && infile >> aux_init
+			 && infile >> ch && ch == delimiter && infile >> aux_fin){
+
+		IDs = aux_s;
+		init_pos = aux_init;
+		fin_pos = aux_fin;
+	}
+	
+	else{
+		 state = BAD_QUERY;
+		 return;
+	}
+
+}
+
+		
 
 
 #endif
